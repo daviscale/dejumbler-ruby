bdcda0c1101f2dd3315e3fc34a82121f6e7aa550
Cale Davis <daviscale@gmail.com>
2022-08-23 11:25:19 -0400

    all controller actions working now

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,8 @@
 class WordsController < ApplicationController
   def index
   end
+
+  def dejumble
+    render json: { foo: :bar }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'words/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "index", to: "words#index"
+  get "dejumble", to: "words#dejumble"
 
   # Defines the root path route ("/")
   root "words#index"
