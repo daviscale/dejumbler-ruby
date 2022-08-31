445bfbd21d589d8372409f268354dce9733d6d8d
Cale Davis <daviscale@gmail.com>
2022-08-31 06:14:18 -0400

    completed initial set of spec

--- spec/requests/words_spec.rb
+++ spec/requests/words_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Words", type: :request do
-  describe "GET /index" do
+  describe "GET /words" do
     it "returns http success" do
-      get "/words/index"
+      get "/words"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /" do
+    it "returns http success" do
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end
--- spec/views/words/index.html.erb_spec.rb
+++ spec/views/words/index.html.erb_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "words/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "on initial page load" do
+    it "displays a form" do
+      assign(:words, [])
+      render
+
+      expect_form(rendered)
+    end
+  end
+
+  context "after dejumbling a word" do
+     it "renders words and displays a form" do
+      assign(:words, ["hello", "world"].map{ |w| Word.new(word: w) })
+      render
+
+      expect(rendered).to match /hello/
+      expect(rendered).to match /world/
+      expect_form(rendered)
+     end
+  end
+
+  def expect_form(rendered)
+    expect(rendered).to match /form action=\"\/words\"/
+    expect(rendered).to match /Dejumble a word/
+    expect(rendered).to match /jumbled word/
+  end
 end
